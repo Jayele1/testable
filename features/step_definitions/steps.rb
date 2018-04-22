83dc02cf1c34d29b24c971dff9d4a964e26f8d23
Jayele1 <JosephAyele@gmail.com>
2018-04-22 13:41:43 -0400

    Added some comments

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -8,6 +8,7 @@ end
 
 Then /^I complete sign up$/ do
 	fill_in('user_name', :with => 'Joseph Ayele')
+	#Added a random number to the email string so that you can run the test with new accounts each time
 	fill_in('user_email', :with => 'JosephAyele+'+rand(10000).to_s+'@gmail.com')
 	fill_in('user_password', :with => 'test12')
 	fill_in('user_password_confirmation', :with => 'test12')
@@ -21,6 +22,7 @@ Then /^I complete the course$/ do
 	click_link('Continue to Course')
 	click_link('Start next lecture')
 	click_link('lecture_complete_button')
+	# Added a sleep function here so the button would load and be clickable
 	sleep(1)
 	click_link('lecture_complete_button')
 	sleep(1)
@@ -30,6 +32,7 @@ Then /^I complete the course$/ do
 end
 
 Then /^I should see the course as 100% complete$/ do 
+	#Checking for the text 100% complete 
 	raise "Fail" if page.has_content?("100\% COMPLETE") == false
 	
 end
